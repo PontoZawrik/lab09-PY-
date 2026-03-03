40511267f3300f69e107fe07cd783121ce8f2917
PontoZawrik <Psichotroniks@tut.by>
2026-03-03 22:05:21 +0300

    Задание 8

--- modules/main.py
+++ modules/main.py
@@ -1,6 +1,22 @@
 from module import suml, prodl
+from square import *
 
 zeroes = [0 for i in range(5)]
 ones = [1 for i in range(5)]
 print(suml(zeroes))
-print(prodl(ones))
+print(prodl(ones), end="\n\n")
+
+
+print("1) Прямоугольник\n2) Треугольник\n3) Круг")
+p = int(input("Площадь какой фигуры вы хотите вычислить: "))
+
+if p == 1:
+    a, b = map(int, input("Введите длину и ширину прямоугольника: ").split())
+    print(f"Площадь прямоугольника: {rectangle_area(a, b)}")
+elif p == 2:
+    a, h = map(int, input("Введите основание и высоту треугольника: ").split())
+    print(f"Площадь тремоугольника: {triangle_area(a, h)}")
+elif p == 3:
+    radius = int(input("Введите радиус круга: "))
+    print(f"Площадь круга: {circle_area(radius)}")
+
---
+++ modules/square.py
@@ -0,0 +1,11 @@
+from math import floor
+
+
+def rectangle_area(a, b):
+    return a * b
+
+def triangle_area(a, h):
+    return round(a * h / 2, 3)
+
+def circle_area(radius):
+    return round(3.1415 * radius ** 2, 3)
